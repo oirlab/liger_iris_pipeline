b6c29131ae1107056a6fa4720c41bf9dc46bcb12
Bryson Cale <bryson.cale1@gmail.com>
2025-03-26 16:53:23 -0700

    Disable merge subarrays test until ASDF in FITS is better understood since testing data products utilize this format

--- liger_iris_pipeline/tests/test_merge_subarrays.py
+++ liger_iris_pipeline/tests/test_merge_subarrays.py
@@ -6,32 +6,35 @@ import numpy as np
 import json
 
 
-def test_merge_subarrays(tmp_path):
 
-    reduced_science_frame_filename = '/Users/cale/Desktop/Liger_IRIS_Test_Data_OLD/reduced_science_frame_sci_with_subarrays_20240831.fits'
-    reduced_subarray1_filename = '/Users/cale/Desktop/Liger_IRIS_Test_Data_OLD/reduced_science_frame_sci_subarray_1_20240831.fits'
-    reduced_subarray2_filename = '/Users/cale/Desktop/Liger_IRIS_Test_Data_OLD/reduced_science_frame_sci_subarray_2_20240831.fits'
+# This test works locally, but these files are not stored in the Liger_IRIS_Test_Data repo until the 
+# "Embedding ASDF in FITS" is better understood
+# def test_merge_subarrays(tmp_path):
 
-    # ASN
-    asn = L1Association.from_product({
-        "members": [
-            {
-                "expname": reduced_science_frame_filename,
-                "exptype": "SCI",
-            },
-            {
-                "expname": reduced_subarray1_filename,
-                "exptype": "SCI"
-            },
-            {
-                "expname": reduced_subarray2_filename,
-                "exptype": "SCI"
-            }
-        ]
-    })
+#     reduced_science_frame_filename = '/Users/cale/Desktop/Liger_IRIS_Test_Data_OLD/reduced_science_frame_sci_with_subarrays_20240831.fits'
+#     reduced_subarray1_filename = '/Users/cale/Desktop/Liger_IRIS_Test_Data_OLD/reduced_science_frame_sci_subarray_1_20240831.fits'
+#     reduced_subarray2_filename = '/Users/cale/Desktop/Liger_IRIS_Test_Data_OLD/reduced_science_frame_sci_subarray_2_20240831.fits'
 
-    # Run the merge subarray step
-    result = MergeSubarraysStep().run(asn)
+#     # ASN
+#     asn = L1Association.from_product({
+#         "members": [
+#             {
+#                 "expname": reduced_science_frame_filename,
+#                 "exptype": "SCI",
+#             },
+#             {
+#                 "expname": reduced_subarray1_filename,
+#                 "exptype": "SCI"
+#             },
+#             {
+#                 "expname": reduced_subarray2_filename,
+#                 "exptype": "SCI"
+#             }
+#         ]
+#     })
 
-    # Check that the image is valid
-    assert np.all(np.logical_not(np.isnan(result.data)))
+#     # Run the merge subarray step
+#     result = MergeSubarraysStep().run(asn)
+
+#     # Check that the image is valid
+#     assert np.all(np.logical_not(np.isnan(result.data)))
