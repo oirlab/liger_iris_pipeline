979e7592da1132f4aa0243bc7d4af33ed4256f0f
Bryson Cale <bryson.cale1@gmail.com>
2025-07-08 13:34:29 -0700

    Deleted associations and extern dirs

--- liger_iris_pipeline/_version.py
+++ liger_iris_pipeline/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.4.0.post190+unknown'
-__version_tuple__ = version_tuple = (0, 4, 0, 'post190', 'unknown')
+__version__ = version = '0.4.0.post196+unknown'
+__version_tuple__ = version_tuple = (0, 4, 0, 'post196', 'unknown')
