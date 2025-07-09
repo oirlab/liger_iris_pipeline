4e0e9bc7a979220bd6ae49fbdad0c0d43181a6c7
Bryson Cale <bryson.cale1@gmail.com>
2025-07-08 19:41:02 -0700

    Lazily loading all datamodel attrs for now

--- liger_iris_pipeline/datamodels/imager.py
+++ liger_iris_pipeline/datamodels/imager.py
@@ -2,23 +2,8 @@ from .model_base import LigerIRISDataModel
 
 __all__ = ["ImagerModel"]
 
-
 class ImagerModel(LigerIRISDataModel):
     """
     A data model for the typical data product from the Liger or IRIS imager (single detector).
-
-    Parameters:
-    data (np.ndarray): The science data array.
-    err (np.ndarray): : The error array.
-    dq (np.ndarray): 2D data quality array.
     """
-
     schema_url = "https://oirlab.github.io/schemas/ImagerModel.schema"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Implicitly create arrays
-        self.data = self.data
-        self.err = self.err
-        self.dq = self.dq
